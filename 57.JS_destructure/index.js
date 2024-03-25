37a98c18b88dad45be624dde0522371ab10bbfa1
Supriyo Das <das.supriyo07@gmail.com>
2024-03-25 22:39:58 +0530

    first commit

---
+++ 57.JS_destructure/index.js
@@ -0,0 +1,32 @@
+// Destructure Array, Object and function parameter
+
+// Destructure Array
+let numberArray = [10, 20, 30, 40];
+let [n1, n2, n3, n4] = numberArray;
+console.log(n1);
+console.log(n2);
+
+// Destructure Object
+let student = {
+    id : 101,
+    fullName : 'Supriyo Das',
+    gpa : 3.53
+}
+
+const {id, fullName, gpa} = student;
+
+console.log(id);
+console.log(fullName);
+
+//Destructure Function parameter
+const studentData = ({roll, studentName, cgpa}) => {
+    console.log(`${roll} , ${studentName}, ${cgpa}`);
+}
+
+const students = {
+    roll : 1,
+    studentName : 'Rupa Das',
+    cgpa : 3.44
+}
+
+studentData(students);
