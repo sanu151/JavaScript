375311b7bef01ab8c34f71fcfdcca15c4acb62f1
Supriyo Das <das.supriyo07@gmail.com>
2024-05-03 23:04:37 +0530

    Functions

---
+++ scaler_tutorial/function/expression.js
@@ -0,0 +1,9 @@
+// Function as Expression
+
+var add = function (a, b) {
+  return a + b;
+};
+
+var result = add(4, 5);
+
+console.log(result);
---
+++ scaler_tutorial/function/function.js
@@ -0,0 +1,11 @@
+function greet() {
+  console.log("Hello World");
+}
+
+greet();
+
+function add(a, b) {
+  console.log(a + b);
+}
+
+add(3, 5);
