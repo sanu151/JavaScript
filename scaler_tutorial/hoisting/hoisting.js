098fba9225ade3b06eebe02f43dc5dd905676abc
Supriyo Das <das.supriyo07@gmail.com>
2024-05-06 05:18:08 +0530

    Hoisting

---
+++ scaler_tutorial/hoisting/hoisting.js
@@ -0,0 +1,15 @@
+console.log(a); // if 'a' define leter it will print 'undefined', if we not define 'a' leter, it will get error : a is not defined,
+greet(); // Welcome Supriyo
+add(2, 4); // Error : add is not a function
+
+var a = "hi"; // defining and assigning 'a'
+
+// function
+function greet() {
+  console.log("Welcome Supriyo");
+}
+
+// Expression
+var add = function (a, b) {
+  console.log(a + b);
+};
