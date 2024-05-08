a955fe07ed4320210f7b7755edd0959f1a5fa47f
Supriyo Das <das.supriyo07@gmail.com>
2024-05-08 06:31:25 +0530

    es6-> Modules and Modularity

--- scaler_tutorial/ES6/modules/calc.js
+++ scaler_tutorial/ES6/modules/calc.js
@@ -1,8 +1,10 @@
+// Create modules
 const add = (a, b) => console.log(a + b);
 const sub = (a, b) => console.log(a - b);
 const mul = (a, b) => console.log(a * b);
 const div = (a, b) => console.log(a / b);
 
+// export modules
 module.exports = {
   addition: add,
   subtruction: sub,
--- scaler_tutorial/ES6/modules/main.js
+++ scaler_tutorial/ES6/modules/main.js
@@ -1,5 +1,7 @@
+// Modules and Modularity
 const calc = require("./calc");
 
+// Calling functions by modules key_name
 calc.addition(4, 6);
 calc.subtruction(12, 5);
 calc.multiplication(3, 9);
