7cbbb7403d227f014396678286a8eaed8dd07f05
Supriyo Das <das.supriyo07@gmail.com>
2024-06-02 18:11:20 +0530

    mislenious -> comparison operator

---
+++ scaler_tutorial/mislenious/comparison.js
@@ -0,0 +1,7 @@
+// Comprarison operator
+
+let a = 2;
+let b = "2";
+
+console.log(a == b); // loose checking (only check the value not check data type)
+console.log(a === b); // strict checking (Check both value and data type)
