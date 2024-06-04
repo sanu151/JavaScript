6514918aa185fcbe147dad0742806c357842ce8d
Supriyo Das <das.supriyo07@gmail.com>
2024-06-04 22:44:48 +0530

    mislenious -> typeof operator

---
+++ scaler_tutorial/mislenious/typeOf.js
@@ -0,0 +1,17 @@
+let num = 2;
+console.log(typeof num);
+
+let str = "Supriyo";
+console.log(typeof str);
+
+let bool = true;
+console.log(typeof bool);
+
+let myObj = {
+  name: "Sanu",
+  age: 36,
+};
+console.log(typeof myObj);
+
+let arr = [1, 2, 3];
+console.log(typeof arr);
